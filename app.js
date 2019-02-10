296757bbe8d2e94e661307d204b7b87c4e16a949
eboxyz <eboxyz@gmail.com>
2019-02-09 18:06:42 -0800

    make bunch of changes

--- app.js
+++ app.js
@@ -3,7 +3,24 @@
 require('dotenv').config();
 const smartcar = require('smartcar');
 const express = require('express');
+const session = require('cookie-session');
+const url = require('url');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 var app = express();
+
+app.use(session({
+    name: 'smartcarproj',
+    secret: 'smartcarproj-super-secret-secret'
+}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
 
 const port = process.env.PORT;
 
@@ -11,7 +28,6 @@ const client = new smartcar.AuthClient({
     clientId: `${process.env.CLIENTID}`,
     clientSecret: `${process.env.CLIENTSECRET}`,
     redirectUri: `http://localhost:${process.env.PORT}/callback`,
-    scope: ['read_vehicle_info'],
     testMode: true,
 });
 
@@ -30,38 +46,129 @@ app.get('/callback', (req, res, next) => {
     return client.exchangeCode(req.query.code)
         .then((_access) => {
             access = _access;
-            return smartcar.getVehicleIds(access.accessToken);
+            req.session = {};
+            req.session.vehicles = {};
+            req.session.access = access;
+            return res.redirect('/vehicles');
         })
-        .then((res) => {
-            const vehicle = new smartcar.Vehicle(res.vehicles[0], access.accessToken);
-            return vehicle.info();
-        })
-        .then((data) => {
-            console.log(data);
-            res.json(data);
-        });
 });
 
-app.get('/hello', (req, res, next) => {
-    let access;
+app.get('/vehicles', (req, res, next) => {
+    const { access, vehicles } = req.session;
+    if (!access) {
+        return res.redirect('/login');
+    }
+    const { accessToken } = access;
+    smartcar.getVehicleIds(accessToken)
+        .then((data) => {
+            const vehicleIds = data.vehicles;
+            const vehiclePromises = vehicleIds.map(vehicleId => {
+                const vehicle = new smartcar.Vehicle(vehicleId, accessToken);
+                req.session.vehicles[vehicleId] = {
+                    id: vehicleId,
+                };
+                return vehicle.info();
+            });
 
-    if (req.query.error) {
-        return next(new Error(req.query.error));
+            return Promise.all(vehiclePromises)
+                .then((data) => {
+                    data.map(vehicle => {
+                        const { id: vehicleId } = vehicle;
+                        req.session.vehicles[vehicleId] = vehicle;
+                    })
+                    res.render('vehicles', { vehicles: req.session.vehicles });
+
+                })
+                .catch((err) => {
+                    const message = err.message || 'Failed to get vehicle info';
+                    const action = 'fetching vehicle info';
+                    return redirectToError(res, message, action);
+                });
+        })
+})
+
+app.post('/request', (req, res, next) => {
+    const { access, vehicles } = req.session;
+    if (!access) {
+        return res.redirect('/login');
     }
 
-    return client.exchangeCode(req.query.code)
-        .then((_access) => {
-            access = _access;
-            return smartcar.getVehicleIds(access.accessToken);
-        })
-        .then((res) => {
-            const vehicle = new smartcar.Vehicle(res.vehicles[0], access.accessToken);
-            return vehicle.info();
-        })
-        .then((data) => {
-            console.log(data);
-            res.json(data);
-        });
+    const { vehicleId, requestType: type } = req.body;
+    const vehicle = vehicles[vehicleId];
+    const instance = new smartcar.Vehicle(vehicleId, access.AccessToken);
+
+    let data = null;
+
+    switch (type) {
+        case 'info':
+            instance.info()
+                .then(data => res.render('data', { data, type, vehicle }))
+                .catch(function (err) {
+                    const message = err.message || 'Failed to get vehicle info.';
+                    const action = 'fetching vehicle info';
+                    return redirectToError(res, message, action);
+                });
+            break;
+        case 'lock':
+            instance.lock()
+                .then(() => {
+                    res.render('data', {
+                        data: {
+                            action: 'Lock request sent.',
+                        },
+                        type,
+                        vehicle,
+                    });
+                })
+                .catch((err) => {
+                    const message = err.message || 'Failed to send lock request to vehicle.'
+                    const action = 'locking vehicle';
+                    return redirectToError(res, message, action);
+                });
+            break;
+        case 'unlock':
+            instance.unlock()
+                .then(() => {
+                    res.render('data', {
+                        vehicle,
+                        type,
+
+                        data: {
+                            action: 'Unlock request sent.',
+                        },
+                    });
+                })
+                .catch((err) => {
+                    const message = err.message || 'Failed to send unlock request to vehicle.'
+                    const action = 'unlocking vehicle';
+                    return redirectToError(res, message, action);
+                });
+            break;
+        default:
+            return redirectToError(
+                res,
+                `Failed to find request type ${requestType}`,
+                'sending request to vehicle'
+            );
+    }
+})
+
+
+const redirectToError = (res, message, action) => res.redirect(url.format({
+    pathname: '/error',
+    query: { message, action },
+}));
+
+
+app.get('/error', function (req, res, next) {
+
+    const { action, message } = req.query;
+    if (!action && !message) {
+        return res.redirect('/');
+    }
+
+    res.render('error', { action, message });
+
 });
 
 app.listen(port, () => console.log(`server running on port: ${port}`));
